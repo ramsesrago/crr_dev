06c1d394457be769eb8216895add22bbe8cdebc4
Rodrigo Flores Carrasco <rodrigofc.personal@outlook.com>
2021-01-07 22:56:24 -0600

    Adding delete and display functions

--- Classes/Classes/miniphonedir.cpp
+++ Classes/Classes/miniphonedir.cpp
@@ -1,76 +1,79 @@
 #include<iostream> 
+#include<map>
 
-class UserData { 
+class ContactData { 
 
 private: 
 
-    string name; 
-
-    string last_name; 
-
-    string email; 
-
-    string phone_number; 
-
-    /*Methods*/ 
-
+    std::string _name;
+    std::string _last_name;
+    std::string _email;
+    std::string _phone_number;
+   
 public: 
 
-    void set_name(string name_temp); 
-
-    string get_name(); 
+    ContactData(std::string name, std::string last_name, std::string email, std::string phone_number){
+        _name = name;
+        _phone_number = phone_number;
+        _email = email;
+        _last_name = last_name;
+    }
+    
+    std::string get_name(void){
+        return _name;
+    }
 }; 
 
-UserData* new_node()
-{   
-    
-     UserData *ptr = new UserData();
-     return ptr;
-    }  
+class Organizer {
+    private:
+        std::map<int, ContactData*> map;
+        int _id=0;
 
-void UserData::set_name(string name_temp) 
+    public: 
+       void create_contact(std::string name, std::string last_name, std::string email, std::string phone_number) {
+            ContactData* contact = new ContactData(name,last_name,email,phone_number);  
+            map[_id] = contact;
+            ++_id;
+       }
 
-{ 
+       bool delete_contact(int id) {
+        bool found=false;
+        for (auto& it : map) {
+               if(id==it.first)
+                {
+                    map.erase(id);
+                    delete(it.second);
+                    found=true;
+                }
+                    
+            }   
+            return found;
+       }
 
-    name = name_temp; 
+       void display_contacts(void) {
+           for (auto& it : map) {
+               std::cout << "The name of the contact for id: " << it.first << " is: " << it.second->get_name() << std::endl;
+            }
+       }
+    };
 
-} 
-
-string UserData::get_name() 
-
-{ 
-
-    return name; 
-
-} 
-
-void UserData::set_last_name(string name_temp) 
-
-{ 
-
-    last_name = name_temp; 
-
-} 
-
-string UserData::get_name() 
-{
-    return name; 
-} 
 
 int main(void) 
-
 { 
 
-    int option=0; 
-
-    UserData *ptr_node;
-    string name; 
-
+    int option=0,id=0;
+    bool found;
+    Organizer org;
+    std::string name, last_name, email, phone_number;
+ 
     do { 
 
         std::cout << "Directorio Telefonico\n"; 
         std::cout << "Seleccione una opcion:\n\n";
         std::cout << "1. Agregar Nuevo\n\n"; 
+        std::cout << "2. Imprimir contactos\n\n"; 
+        std::cout << "3. Eliminar Contacto\n\n"; 
+        std::cout << "4. Salir\n\n"; 
         std::cout << "Seleccione:"; 
         std::cin >> option; 
 
@@ -78,17 +81,39 @@ int main(void)
         switch (option) 
         { 
 
-        case 1:  
-            ptr_node = new_node();
-            system("cls"); 
-            UserData user_new;
-            cout << "Nombre:"; 
-            cin >> name; 
-            User_New.set_name(name); 
-            cout << User_New.get_name(); 
-            system("pause"); 
-
-        } 
+        case 1:
+            system("clear");
+            std::cout << "Nombre:"; 
+            std::cin >> name; 
+            std::cout << "Apellido:"; 
+            std::cin >> last_name; 
+            std::cout << "Email:"; 
+            std::cin >> email; 
+            std::cout << "Telefono:"; 
+            std::cin >> phone_number; 
+            org.create_contact(name,last_name,email,phone_number);
+            std::cout << "Contacto nuevo guardado con exito";
+            system("clear");
+            break;
+            
+        case 2:
+            system("clear");
+            org.display_contacts();
+            system("clear");
+            break;
+        
+        case 3:
+            system("clear");
+            std::cout << "Introduce ID a eliminar:"; 
+            std::cin >> id; 
+            found=org.delete_contact(id);
+            if(found)
+                std::cout << "Elemento encontrado y eliminado"; 
+            else
+                std::cout << "Elemento no encontrado"; 
+            system("clear");
+            break;
+        }
 
     } while (option != 4); 
 

e32785e2fb1e50005ed8bb1f330d3fad5c93859f
Rodrigo Flores <rodrigofc.personal@outlook.com>
2021-01-20 18:33:20 -0600

    Enabling singleton organizer and update delete function

--- Classes/Classes/miniphonedir.cpp
+++ Classes/Classes/miniphonedir.cpp
@@ -24,12 +24,24 @@ public:
     }
 }; 
 
-class Organizer {
+class Singleton_Organizer {
     private:
+        Singleton_Organizer()
+        {
+            std::cout<<"Creating organizer" << std::endl;
+        }
         std::map<int, ContactData*> map;
+        std::map<int, ContactData*>::iterator it;
         int _id=0;
 
     public: 
+        /*Getting singleton instance*/
+        static Singleton_Organizer& get_organizer(){
+            std::cout<<"Getting organizer from singleton"<<std::endl;
+            static Singleton_Organizer org;
+            return org;
+        }
+
        void create_contact(std::string name, std::string last_name, std::string email, std::string phone_number) {
             ContactData* contact = new ContactData(name,last_name,email,phone_number);  
             map[_id] = contact;
@@ -37,20 +49,21 @@ class Organizer {
        }
 
        bool delete_contact(int id) {
-        bool found=false;
-        for (auto& it : map) {
-               if(id==it.first)
-                {
-                    map.erase(id);
-                    delete(it.second);
-                    found=true;
-                }
+            it = map.find(id);
+            if (it != map.end()){
+                map.erase (it);   
+                std::cout<< "ID: " << id << "was deleted" <<std::endl;
+            }
+            else
+            {
+                std::cout<<"Error: Element not found"<<std::endl;
+            }
                     
-            }   
-            return found;
+        
        }
 
        void display_contacts(void) {
+            std::cout << "Displaying contacts ...";
            for (auto& it : map) {
                std::cout << "The name of the contact for id: " << it.first << " is: " << it.second->get_name() << std::endl;
             }
@@ -63,7 +76,9 @@ int main(void)
 
     int option=0,id=0;
     bool found;
-    Organizer org;
+    /*ToDo Singleton Organizer*/
+    /*Organizer org;*/
+    Singleton_Organizer& org = Singleton_Organizer::get_organizer();
     std::string name, last_name, email, phone_number;
  
     do { 
@@ -93,26 +108,21 @@ int main(void)
             std::cin >> phone_number; 
             org.create_contact(name,last_name,email,phone_number);
             std::cout << "Contacto nuevo guardado con exito";
-            system("clear");
+            
             break;
             
         case 2:
             system("clear");
             org.display_contacts();
-            system("clear");
+            
             break;
         
         case 3:
             system("clear");
-            std::cout << "Introduce ID a eliminar:"; 
+            std::cout << "Introduce ID a eliminar:"<< std::endl; 
             std::cin >> id; 
-            found=org.delete_contact(id);
-            if(found)
-                std::cout << "Elemento encontrado y eliminado"; 
-            else
-                std::cout << "Elemento no encontrado"; 
-            system("clear");
-            break;
+            org.delete_contact(id);
+            
         }
 
     } while (option != 4); 
